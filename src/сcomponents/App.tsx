05093ecf3da091caed47adf542972e855e2ee1c9
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-09-19 20:19:07 +0200

    upd

---
+++ src/сcomponents/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+import Mailbox from "./Mailbox";
+
+export default function App() {
+  return (
+    <>
+      <h1>Best selling</h1>
+      <Mailbox username="username" />
+    </>
+  );
+}
