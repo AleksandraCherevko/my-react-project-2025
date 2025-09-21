f6dbbdb29a725297074970e0e12fe6bf69c3456b
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-09-21 20:13:30 +0200

    add css

--- src/сcomponents/App.tsx
+++ src/сcomponents/App.tsx
@@ -1,11 +1,22 @@
-// src/App.tsx
-import Mailbox from "./Mailbox";
+import Product from "./Product";
+import Button from "./Button";
 
 export default function App() {
   return (
     <>
       <h1>Best selling</h1>
-      <Mailbox username="username" />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
+        price={14.29}
+      />
+      <Button variant="primary" text="Login" />
+      <Button variant="secondary" text="Follow" />
     </>
   );
 }
